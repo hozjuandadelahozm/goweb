ccd62f7fbcd61c542463d030819819ce2dcc64e8
juan.delahoz <juan.delahoz@silvercredit.co>
2023-03-08 23:40:27 -0500

    novena parte

--- 04-go-postgres/main.go
+++ 04-go-postgres/main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"gopostgres/db"
+	"gopostgres/models"
 )
 
 func main() {
 	db.Connect()
 
-	fmt.Println(db.ExistsTable("users"))
+	// fmt.Println(db.ExistsTable("users"))
 	// db.CreateTable(models.UserSchema, "users")
 
-	db.TruncateTable("users")
+	// user := models.CreateUser("rosmeri", "9635", "rosmeri@gmail.com")
+	// fmt.Println(user)
+
+	// users := models.ListUsers()
+	// fmt.Println(users)
+
+	user := models.GetUser(4)
+	fmt.Println(user)
+
+	// db.TruncateTable("users")
 	db.Close()
 	// db.Ping()
 }
--- 04-go-postgres/models/users.go
+++ 04-go-postgres/models/users.go
@@ -1,16 +1,68 @@
 package models
 
+import "gopostgres/db"
+
 type User struct {
-	Id       int
+	Id       int64
 	Username string
 	Password string
 	Email    string
 }
 
-const UserSchema string = `CREATE TABLE users (
-	id	SMALLINT NOT NULL PRIMARY KEY,
-	username VARCHAR(30) NOT NULL,
-	password VARCHAR(100) NOT NULL,
-	email VARCHAR(50),
-	create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)`
+type Users []User
+
+const UserSchema string = `create table users(
+	id serial not null primary key,
+	username varchar(30) not null,
+	password varchar(100) not null,
+	email varchar(50),
+	create_data timestamp default current_timestamp
+  );`
+
+// Construir usuario
+func NewUser(username, password, email string) *User {
+	user := &User{Username: username, Password: password, Email: email}
+	return user
+}
+
+//Crear usuario e insertar bd
+func CreateUser(username, password, email string) *User {
+	user := NewUser(username, password, email)
+	user.insert()
+	return user
+}
+
+// Insertar Registro
+func (user *User) insert() {
+	sql := "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)"
+	result, _ := db.Exec(sql, user.Username, user.Password, user.Email)
+	user.Id, _ = result.LastInsertId()
+}
+
+// Listar todos los registros
+func ListUsers() Users {
+	sql := "SELECT id, username, password, email FROM users"
+	users := Users{}
+	rows, _ := db.Query(sql)
+
+	for rows.Next() {
+		user := User{}
+		rows.Scan(&user.Id, &user.Username, &user.Password, &user.Email)
+		users = append(users, user)
+	}
+
+	return users
+}
+
+//Obtener un registro
+func GetUser(id int) *User {
+	user := NewUser("", "", "")
+
+	sql := "SELECT id, username, password, email FROM users WHERE id = $1"
+	rows, _ := db.Query(sql, id)
+
+	for rows.Next() {
+		rows.Scan(&user.Id, &user.Username, &user.Password, &user.Email)
+	}
+	return user
+}
