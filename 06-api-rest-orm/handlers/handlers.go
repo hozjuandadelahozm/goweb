5d3e773d77b79734225084c2f3aa19af47c1a292
Juan David De la hoz Montero <juan.delahoz@silvercredit.co>
2023-03-14 15:14:53 -0500

    parte 14

--- 06-api-rest-orm/handlers/handlers.go
+++ 06-api-rest-orm/handlers/handlers.go
@@ -1,9 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
 	"gorm/db"
 	"gorm/models"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request) {
@@ -15,28 +20,69 @@ func GetUsers(rw http.ResponseWriter, r *http.Request) {
 
 func GetUser(rw http.ResponseWriter, r *http.Request) {
 
+	if user, err := getUserById(r); err != nil {
+		sendError(rw, http.StatusNotFound)
+	} else {
+		senData(rw, user, http.StatusOK)
+	}
+}
+
+func getUserById(r *http.Request) (models.User, *gorm.DB) {
+	//Obtener ID
+	vars := mux.Vars(r)
+	userId, _ := strconv.Atoi(vars["id"])
+	user := models.User{}
+
+	if err := db.Database.First(&user, userId); err.Error != nil {
+		return user, err
+	} else {
+		return user, nil
+	}
 }
 
 func CreateUser(rw http.ResponseWriter, r *http.Request) {
 
+	//Obtener registro
+	user := models.User{}
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&user); err != nil {
+		sendError(rw, http.StatusUnprocessableEntity)
+	} else {
+		db.Database.Save(&user)
+		senData(rw, user, http.StatusCreated)
+	}
 }
 
 func UpdateUser(rw http.ResponseWriter, r *http.Request) {
 
+	//Obtener registro
+	var userId int64
+
+	if user_ant, err := getUserById(r); err != nil {
+		sendError(rw, http.StatusNotFound)
+	} else {
+		userId = user_ant.Id
+
+		user := models.User{}
+		decoder := json.NewDecoder(r.Body)
+
+		if err := decoder.Decode(&user); err != nil {
+			sendError(rw, http.StatusUnprocessableEntity)
+		} else {
+			user.Id = userId
+			db.Database.Save(&user)
+			senData(rw, user, http.StatusOK)
+		}
+	}
 }
 
 func DeleteUser(rw http.ResponseWriter, r *http.Request) {
 
+	if user, err := getUserById(r); err != nil {
+		sendError(rw, http.StatusNotFound)
+	} else {
+		db.Database.Delete(&user)
+		senData(rw, user, http.StatusOK)
+	}
 }
-
-// func getUserByRequest(r *http.Request) (models.User, error) {
-// 	//Obtener ID
-// 	vars := mux.Vars(r)
-// 	userId, _ := strconv.Atoi(vars["id"])
-
-// 	if user, err := models.GetUser(userId); err != nil {
-// 		return *user, err
-// 	} else {
-// 		return *user, nil
-// 	}
-// }
--- 06-api-rest-orm/main.go
+++ 06-api-rest-orm/main.go
@@ -17,10 +17,10 @@ func main() {
 
 	//EndPoint
 	mux.HandleFunc("/api/user/", handlers.GetUsers).Methods("GET")
-	// mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.GetUser).Methods("GET")
-	// mux.HandleFunc("/api/user/", handlers.CreateUser).Methods("POST")
-	// mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
-	// mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.GetUser).Methods("GET")
+	mux.HandleFunc("/api/user/", handlers.CreateUser).Methods("POST")
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":4000", mux))
 }
